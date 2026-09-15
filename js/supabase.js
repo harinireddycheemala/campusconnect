ff9f2e7eaf5473fa97ac69a7f9f8462794b1e2d8
harinireddycheemala <harinireddycheemala@gmail.com>
2026-09-15 23:01:07 +0530

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://kwvwrjpsophoczihyzpp.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_g72UBOMYiTTmXzxvD0wgFQ_4ntGq6gB";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
